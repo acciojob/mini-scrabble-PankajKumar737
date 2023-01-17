25bb251f23bd54c71ba22584181c85d1e69d3a8a
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-01-17 18:00:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText");
+let letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener("input", calculateLength);
+
+function calculateLength(){
+	let word = evaluatedText.value;
+	letterCount.innerHTML = word.length;
+}
